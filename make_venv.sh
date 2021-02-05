b71a3b4c8eab84316a9f342af88c4c013c370f53
Karl Bartel <karl@karl.berlin>
2021-02-05 19:01:36 +0100

    Initial commit

---
+++ make_venv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python3 -m venv venv
+source venv/bin/activate
+
+# pip install git+git://github.com/karlb/wikdict-web
+pip install -e ~/code/github/wikdict-web/
+
+CFLAGS='-DSQLITE_ENABLE_ICU' CPPFLAGS=$(pkg-config --cflags icu-uc icu-uc icu-i18n) LDFLAGS=$(pkg-config --libs icu-uc icu-uc icu-i18n) venv/bin/pip install git+git://github.com/karlb/pysqlite3
+pip install sqlite-spellfix
